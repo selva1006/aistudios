03d7f5c86dae1d1a76e8bc243e436cf519c9df90
selva1006 <selva@kalpanikfilms.com>
2025-06-14 22:46:54 +0200

    added tools

--- chat_graph.py
+++ chat_graph.py
@@ -1,23 +1,36 @@
 import traceback
+from langgraph.prebuilt import ToolNode, tools_condition
 from langchain.chat_models import init_chat_model
 from langgraph.graph import StateGraph, START
 from langgraph.graph.message import add_messages
 from typing_extensions import TypedDict
 from typing import Annotated
 
+import search_tool
 
 llm = init_chat_model("openai:gpt-4.1")
 
+tools = [search_tool.tool]
+llm_with_tools = llm.bind_tools(tools)
+
 class State(TypedDict):
-    messages:   Annotated[list, add_messages]
+    messages: Annotated[list, add_messages]
+
 
 graph_builder = StateGraph(State)
 
 def chatbot(state: State):
-    return {"messages" : [llm.invoke(state["messages"])]}
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
 
 graph_builder.add_node("chatbot", chatbot)
 graph_builder.add_edge(START, "chatbot")
+
+tools_node = ToolNode(tools)
+graph_builder.add_node("tools", tools_node)
+
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.add_conditional_edges("chatbot", tools_condition)
+
 graph = graph_builder.compile()
 
 def stream_graph_updates(user_inp: str):
@@ -37,6 +50,3 @@ if __name__ == '__main__':
         except Exception:
             print(traceback.format_exc())
             break
-
-
-
---
+++ search_tool.py
@@ -0,0 +1,3 @@
+from langchain_tavily import TavilySearch
+
+tool = TavilySearch(max_results=2)
